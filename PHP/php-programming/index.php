5c779caa9ce7d16d9da7ec8ccd4da1124fd4fc6d
amskz401 <amskz401@gmail.com>
2024-04-26 19:39:30 +0500

    php lecture

---
+++ PHP/php-programming/index.php
@@ -0,0 +1,84 @@
+<?php 
+
+// 
+// OOP (object oriented programming) ==> class, object, inheritance, overload/override, 
+// traits, interfaces, helper
+
+// declare varibales
+// $name = "Farhan";
+// echo $name;
+
+// case-sensitive
+$Name = "Farhan";
+$name = "Ahmad";
+$nAme = "Ali";
+
+// echo ($Name .' '. $name);
+
+// echo $name;
+
+// comment types (single line, multiline)
+# single line
+
+/* line one
+line two */
+
+
+// variable types (string, numeric, boolean[flag], null) ==> objects and arrays
+
+// syntax error, warning, fatal error, logical error
+
+$value1 = (int) "10";
+$value2 = 32.1;
+
+// var_dump($value1);
+// var_dump($value2);
+
+// echo $value1 . $value2;
+
+$student = "Farhan";
+$student2 = "Ahmad";
+
+$students = [];
+
+// echo ($students[0] . $students[1]);
+
+// $student = {
+// 	id: 1,
+// 	name: "Farhan",
+// };
+// $student2 = {
+// 	id: 1,
+// 	name: "Farhan",
+// };
+
+// echo $student;
+
+// operational statements (math), conditional statement (if esle, switch), loops
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title></title>
+ </head>
+ <body>
+ 	<form>
+ 		<table >
+ 			<tr>
+ 				<td>First Name</td>
+ 				<td><input type="" name=""></td>
+ 			</tr>
+ 			<tr>
+ 				<td><label>Email</label></td>
+ 				<td><input type="" name=""></td>
+ 			</tr>
+ 		</table>
+ 		
+ 	</form>
+ </body>
+ </html>
